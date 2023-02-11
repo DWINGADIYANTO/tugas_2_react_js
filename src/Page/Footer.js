55f5b51c727df0d905e7a1859505b00564eff858
Dwi Ngadiyanto <dwingadiyanto879@gmail.com>
2023-02-11 16:28:12 +0700

    Tugas 2 React.js
    Tugas ini kami kumpulkan untuk Niomic

---
+++ src/Page/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <h3>tugas_2_react_js</h3>
+      </div>
+    );
+  }
+}
+
+export default Footer;
